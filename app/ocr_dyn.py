38c9d4203c9e991290b1dd92b70a48e13ac6fe23
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-01 11:42:20 +0000

    add app/ocr_dyn.py

--- app/ocr_dyn.py
+++ app/ocr_dyn.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+from pathlib import Path
+import os, sys, json, time, io
+from typing import Any, Dict, List, Union
+from PIL import Image

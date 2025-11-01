75e3f7df49a96f34e7fbd2ee1d8feec34d1c3302
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-01 12:59:46 +0000

    add app/scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -30,3 +30,6 @@ except ImportError:
 # =========================================================
 # dynamic session paths
 # =========================================================
+SESSION_DIR = Path(os.getenv("SESSION_DIR", Path.cwd()))
+SOURCE_FOLDER = Path(os.getenv("SOURCE_FOLDER", SESSION_DIR / "uploads"))
+RENAMED_DIR = Path(os.getenv("RENAMED_DIR", SESSION_DIR / "renamed"))

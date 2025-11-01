a62650917a3e9fa1e56c4321189623ceddfb07b8
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-01 12:57:39 +0000

    add app/scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 import os, re, json, time, random, threading, socket, shutil
 from queue import Queue
+from urllib.parse import urljoin, urlparse
+import requests
+from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning
+import warnings

cc52a7434b46aaa625ef212e0d37fdced273707e
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-07 17:42:50 +0000

    update

--- mix_ocr_qr_dyn.py
+++ mix_ocr_qr_dyn.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import os
 import json
 
-# =========================================================
-# 🧩 مسیرهای پویا
+
+#  dynamic paths
 SESSION_DIR = Path(os.getenv("SESSION_DIR", Path.cwd()))
 SOURCE_FOLDER = Path(os.getenv("SOURCE_FOLDER", SESSION_DIR / "uploads"))
 RENAMED_DIR = Path(os.getenv("RENAMED_DIR", SESSION_DIR / "renamed"))

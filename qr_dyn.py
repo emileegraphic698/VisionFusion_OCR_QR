8720069e448ab5521332775d6db769760cd02c06
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-07 17:39:05 +0000

    update

--- qr_dyn.py
+++ qr_dyn.py
@@ -444,7 +444,7 @@ def save_json(path, data):
 
 # ----------------------------------------------------------
 def extract_urls(entry):
-    """استخراج URLها از نتایج"""
+    """extract urls from results"""
     urls = []
     for item in entry.get("result", []):
         link = item.get("qr_link")
